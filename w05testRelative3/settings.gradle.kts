2b94bfe3c1ba1d028e62922b8d5490f77d0dc565
unknown <applespeed550@gmail.com>
2023-10-04 21:43:07 +0900

    5주차 레이아웃

---
+++ w05testRelative3/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "w05testRelative3"
+include(":app")
+ 
